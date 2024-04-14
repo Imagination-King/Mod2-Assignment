5ccf1112099b9f6da73245a19214f45a8ccd26a2
Alex Hamilton <alexhamilton-code@gmail.com>
2024-04-13 21:48:49 -0700

    module 2 assingment take 2

--- test.js
+++ test.js
@@ -3,24 +3,23 @@ console.log("Hello World!")
 /* 3 Examples of basic arithmetic in JS */
 
 console.log(6 * 7) //42
-console.log(8 % 3) //2
-console.log(4 ** 3) //64
+console.log(5 ** 2) //25
+console.log(6 % 3) //0
 
 /* 2 Examples of declaring variables in JS*/
 
 let x = 5
-const y = 6
-var z = 7
+var y = 6
+let z = 3
 console.log(x) //5
 console.log(y) //6
-console.log(z) //7
+console.log(z) //3
 
 /* 2 Examples of using variables in arithmetic operations in JS */
 
-let a = x + 5 
-let b = y ** 2 
-let c = z++
-console.log(a) //10
-console.log(b) //36
-console.log(c) //7
-console.log(z) //8
+let a = x + 5 //10
+let b = y++ //6
+let c = ++y //7
+console.log(a)
+console.log(b)
+console.log(c)
